28fc7237fe68e2cc18af77a233ff9a7480cbedb8
abyad-tif <lapajung50@gmail.com>
2025-09-15 21:41:43 +0800

    navbar navlink

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,18 +7,18 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
-              <x-nav-link href="/" :current="request()->is('/')">
+              <x-my-nav-link href="/" :current="request()->is('/')">
                 Home
-              </x-nav-link>
-              <x-nav-link href="/posts" :current="request()->is('posts')">
+              </x-my-nav-link>
+              <x-my-nav-link href="/posts" :current="request()->is('posts')">
                 Blog
-              </x-nav-link>
-              <x-nav-link href="/about" :current="request()->is('about')">
+              </x-my-nav-link>
+              <x-my-nav-link href="/about" :current="request()->is('about')">
                 About
-              </x-nav-link>
-              <x-nav-link href="/contact" :current="request()->is('contact')">
+              </x-my-nav-link>
+              <x-my-nav-link href="/contact" :current="request()->is('contact')">
                 Contact
-              </x-nav-link>
+              </x-my-nav-link>
             </div>
           </div>
         </div>
@@ -27,11 +27,23 @@
 
             <!-- Profile dropdown -->
             <div class="relative ml-3">
-              <button type="button" @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true"> 
+              @if (Auth::check())
+                <button type="button" @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer" id="user-menu-button" aria-expanded="false" aria-haspopup="true"> 
                 <span class="absolute -inset-1.5"></span>
                 <span class="sr-only">Open user menu</span>
                 <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
-              </button>
+                <div class="ml-3 text-gray-300 text-sm font-medium">{{ Auth::user()->name }}</div>
+                <div class="ms-1 text-gray-300">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                      <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                </div>
+                </button>
+              @else
+                <a href="/login" class="text-white text-sm font-medium">Login</a>
+                <span class="text-white text-sm font-medium">|</span>
+                <a href="/register" class="text-white text-sm font-medium">Register</a>
+              @endif
 
               <div x-show="isOpen" 
               x-transition:enter="transition ease-out duration-100 transform"
@@ -41,9 +53,12 @@
               x-transition:leave-start="opacity-100 scale-100"
               x-transition:leave-end="opacity-0 scale-95"
               class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline-1 outline-black/5 " role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden" role="menuitem" tabindex="-1">Your profile</a>
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden" role="menuitem" tabindex="-1">Settings</a>
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden" role="menuitem" tabindex="-1">Sign out</a>
+                <a href="/profile" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden" role="menuitem" tabindex="-1">Your profile</a>
+                <a href="/dashboard" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden" role="menuitem" tabindex="-1">Settings</a>
+                <form method="POST" action="/logout">
+                  @csrf
+                  <button type="submit" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden cursor-pointer" role="menuitem" tabindex="-1">Log out</button>
+                </form>
               </div>
             </div>
           </div>
@@ -67,34 +82,43 @@
     <el-disclosure x-show="isOpen" id="mobile-menu" class="block md:hidden">
       <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-        <x-nav-link class="block" href="/" :current="request()->is('/')">
+        <x-my-nav-link class="block" href="/" :current="request()->is('/')">
           Home
-        </x-nav-link>
-        <x-nav-link class="block" href="/posts" :current="request()->is('posts')">
+        </x-my-nav-link>
+        <x-my-nav-link class="block" href="/posts" :current="request()->is('posts')">
           Blog
-        </x-nav-link>
-        <x-nav-link class="block" href="/about" :current="request()->is('about')">
+        </x-my-nav-link>
+        <x-my-nav-link class="block" href="/about" :current="request()->is('about')">
           About
-        </x-nav-link>
-        <x-nav-link class="block" href="/contact" :current="request()->is('contact')">
+        </x-my-nav-link>
+        <x-my-nav-link class="block" href="/contact" :current="request()->is('contact')">
           Contact
-        </x-nav-link>
+        </x-my-nav-link>
       </div>
       <div class="border-t border-white/10 pt-4 pb-3">
-        <div class="flex items-center px-5">
-          <div class="shrink-0">
-            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+        @if (Auth::check())
+          <div class="flex items-center px-5">
+            <div class="shrink-0">
+              <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="{{ Auth::user()->name }}" class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+            </div>
+            <div class="ml-3">
+              <div class="text-base/5 font-medium text-white">{{ Auth::user()->name }}</div>
+            </div>
           </div>
-          <div class="ml-3">
-            <div class="text-base/5 font-medium text-white">Tom Cook</div>
-            <div class="text-sm font-medium text-gray-400">tom@example.com</div>
+          <div class="mt-3 space-y-1 px-2">
+            <a href="/profile" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Your profile</a>
+            <a href="/dashboard" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Settings</a>
+            <form method="POST" action="/logout">
+              @csrf
+              <button type="submit" class="block w-full text-left rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white cursor-pointer" role="menuitem" tabindex="-1">Log out</button>
+            </form>
           </div>
-        </div>
-        <div class="mt-3 space-y-1 px-2">
-          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Your profile</a>
-          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Settings</a>
-          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Sign out</a>
-        </div>
+        @else
+          <div class="my-3 space-y-1 px-2">
+            <a href="/login" class="block text-white text-sm font-medium py-2">Login</a>
+            <a href="/register" class="block text-white text-sm font-medium py-2">Register</a>
+          </div>
+        @endif
       </div>
     </el-disclosure>
   </nav>
